b5972e37fe2b9ebbb82ca160fa9af8e22ef4cf33
Alumno <alumno@A51MM-04.local>
2019-02-25 20:31:20 -0500

    update

--- Module03/clase03/manejo del DOM/js/main.js
+++ Module03/clase03/manejo del DOM/js/main.js
@@ -9,11 +9,28 @@ detalle[0].classList.add("cambiado");
 }
 
 btn.addEventListener("mouseenter",tarea1);
-
 function tarea1(){
 	console.log("Entro al elemento");
 }
-
 function mostrar_parrafo(){
 	console.log("click al parrafo");
+}
+
+var ele_inicio=document.getElementById('inicio');
+ele_inicio.addEventListener("click",cambio)
+
+var ele_nosotros=document.getElementById('nosotros');
+ele_nosotros.addEventListener("click",cambio2)
+
+
+function cambio(){
+	ele_inicio.classList.add("activo");
+	ele_nosotros.classList.remove("activo");
+
+}
+
+
+function cambio2(){
+	ele_nosotros.classList.add("activo");
+	ele_inicio.classList.remove("activo");
 }
